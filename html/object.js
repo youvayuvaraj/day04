a992529cc5a82bf905bb431c94f0abd9a0362653
youvayuvaraj <121887488+youvayuvaraj@users.noreply.github.com>
2023-02-01 14:30:38 +0530

    add the theory
    add the theory

---
+++ html/object.js
@@ -0,0 +1,20 @@
+var obj1 = {"name":"Sam","class":"MCA"};
+var obj2 = {"class":"MCA","name":"Sam"};
+
+var flag=true;
+
+if(Object.keys(obj1).length==Object.keys(obj2).length){
+    for(key in obj1) { 
+        if(obj1[key] == obj2[key]) {
+            continue;
+        }
+        else {
+            flag=false;
+            break;
+        }
+    }
+}
+else {
+    flag=false;
+}
+console.log("is object equal"+flag);
